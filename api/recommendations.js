66f3688a5015733c73f31a21c550389342183cf8
samielkahla-png <sami.elkahla@gmail.com>
2026-05-10 19:45:28 +0200

    Create recommendations.js

---
+++ api/recommendations.js
@@ -0,0 +1,4 @@
+# À configurer dans Vercel > Project Settings > Environment Variables
+# Ne mets jamais ces clés directement dans index.html.
+API_FOOTBALL_KEY=your_api_football_key_here
+ODDS_API_KEY=your_the_odds_api_key_here
